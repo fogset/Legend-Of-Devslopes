df5b611ccc586544da1e9e4ac601c0e0735ea3ff
Tianhao Zhang <zhangth1990@hotmail.com>
2023-03-25 19:37:15 -0600

    Implementing enemy attack systems in Unity

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float range = 3f;
+
+    [SerializeField]
+    private float timeBetweenAttacks = 1f;
+
+    private Animator anim;
+
+    private GameObject player;
+
+    private bool playerInRange;
+
+    private BoxCollider[] weaponColliders;
+
+    void Start()
+    {
+        weaponColliders = GetComponentsInChildren<BoxCollider>();
+        player = GameManager.instance.Player;
+        anim = GetComponent<Animator>();
+        StartCoroutine(attack());
+    }
+
+    void Update()
+    {
+        if (
+            Vector3.Distance(transform.position, player.transform.position) <
+            range
+        )
+        {
+            playerInRange = true;
+        }
+        else
+        {
+            playerInRange = false;
+        }
+    }
+
+    IEnumerator attack()
+    {
+        if (playerInRange && !GameManager.instance.GameOver)
+        {
+            anim.Play("Attack");
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+        yield return null;
+        StartCoroutine(attack());
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    [SerializeField]
+    GameObject player;
+
+    private bool gameOver = false;
+
+    public bool GameOver
+    {
+        get
+        {
+            return gameOver;
+        }
+    }
+
+    public GameObject Player
+    {
+        get
+        {
+            return player;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy (gameObject);
+        }
+        DontDestroyOnLoad (gameObject);
+    }
+
+    public void PlayerHit(int currentHP)
+    {
+        if (currentHP > 0)
+        {
+            gameOver = false;
+        }
+        else
+        {
+            gameOver = true;
+        }
+    }
+}
